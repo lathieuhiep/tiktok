5895e66d7e5b24a7cd183a0c9176c66e4bc9e2c6
lakhacdiep <diep.lk@beetechsoft.com>
2022-08-09 17:46:56 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const orders = [100, 200, 300];
+    const [counter, setCounter] = useState(() => {
+        return orders.reduce((total, cur) => total + cur);
+    });
+
+    const handleIncrease = () => {
+        setCounter(prevState => prevState + 1)
+    }
+
+    return (
+        <div className="App" style={{padding: 20}}>
+            <h1>{counter}</h1>
+            <button onClick={handleIncrease}>Increase</button>
+        </div>
+    );
 }
 
 export default App;
