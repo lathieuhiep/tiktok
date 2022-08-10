3ecfaa3f4d69534907610f0b8f4524e197ee9e71
LaThieuHiep <khacdiepkma90@gmail.com>
2022-08-10 22:59:43 +0700

    update

--- src/Content.js
+++ src/Content.js
@@ -1,65 +1,19 @@
-import {useMemo, useRef, useState} from "react";
+import {useState} from "react";
 
 function Content() {
-    const [name, setName] = useState('')
-    const [price, setPrice] = useState('')
-    const [products, setProducts] = useState([])
-
-    const nameRef = useRef({})
-
-    const handleSubmit = () => {
-        setProducts([...products, {
-           name,
-           price: + price
-       }])
-
-        setName('')
-        setPrice('')
-
-        nameRef.current.focus()
-    }
-
-    const total = useMemo(() => {
-
-        const result = products.reduce((result, prod) => {
-            console.log('Tính toán lại')
-
-            return result + prod.price
-        }, 0)
-
-        return result
-
-    }, [products])
+    const [count, setCount] = useState(0)
 
     return (
         <div>
-            <input
-                ref={nameRef}
-                type="text"
-                value={name}
-                placeholder="Enter name..."
-                onChange={e => setName(e.target.value)}
-            />
-            <br/>
-            <input
-                type="text"
-                value={price}
-                placeholder="Enter price..."
-                onChange={e => setPrice(e.target.value)}
-            />
-            <br/>
-            <button onClick={handleSubmit}>Add</button>
-            <br />
+            <h1>{count}</h1>
 
-            <p>Total: {total}</p>
+            <button onClick={() => setCount(count - 1)}>
+                Down
+            </button>
 
-            <ul>
-                {products.map((product, index) => (
-                    <li key={index}>
-                        {product.name} - {product.price}
-                    </li>
-                ))}
-            </ul>
+            <button onClick={() => setCount(count + 1)}>
+                Up
+            </button>
         </div>
     )
 }
