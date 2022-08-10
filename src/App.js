122a3c1a76f636d2f32055368b3054e53d0f93b4
LaThieuHiep <khacdiepkma90@gmail.com>
2022-08-10 21:43:33 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,58 @@
 import {useState} from "react";
 
 function App() {
-    const orders = [100, 200, 300];
-    const [counter, setCounter] = useState(() => {
-        return orders.reduce((total, cur) => total + cur);
-    });
+    const [job, setJob] = useState('')
+    const [jobs, setJobs] = useState(() => {
+        const storageJobs = JSON.parse(sessionStorage.getItem('jobs'))
 
-    const handleIncrease = () => {
-        setCounter(prevState => prevState + 1)
+        return storageJobs ?? []
+    })
+
+    // save sessionStorage
+    const createSessionStorage = (value) => {
+        const jsonJobs = JSON.stringify(value)
+        sessionStorage.setItem('jobs', jsonJobs)
+    }
+
+    const handleSubmit = () => {
+        if ( job ) {
+            setJobs(prevState => {
+                const newJobs = [...prevState, job]
+                createSessionStorage(newJobs)
+
+                return newJobs
+            })
+
+            setJob('')
+        }
+    }
+
+    const handleDelete = (value) => {
+        setJobs(() => {
+            const newJobs = jobs.filter(job => job !== value)
+            createSessionStorage(newJobs)
+
+            return newJobs
+        })
     }
 
     return (
         <div className="App" style={{padding: 20}}>
-            <h1>{counter}</h1>
-            <button onClick={handleIncrease}>Increase</button>
+            <input
+                value={job}
+                onChange={e => setJob(e.target.value)}
+            />
+
+            <button onClick={handleSubmit}>Register</button>
+
+            <ul>
+                {jobs.map((job, index) => (
+                    <li key={index}>
+                        <span>{job}</span>
+                        <button onClick={() => handleDelete(job)}>X</button>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
